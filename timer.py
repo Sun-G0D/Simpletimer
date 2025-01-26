27c79bd739320cd111e18788bbc5319300b76454
Sun-G0D <47151235+Sun-G0D@users.noreply.github.com>
2025-01-26 14:44:17 -0800

    main window
    main window created.

---
+++ timer.py
@@ -0,0 +1,13 @@
+# Import Module
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Timer")
+# Set geometry(widthxheight)
+root.geometry('350x200')
+
+# Execute Tkinter
+root.mainloop()
