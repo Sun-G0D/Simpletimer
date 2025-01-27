89fd0f134572e048762d3bbaefd4d4cae2346656
Sun-G0D <47151235+Sun-G0D@users.noreply.github.com>
2025-01-27 09:55:37 -0800

    Stop button
    is_counting has three states now, 2 is running, 0 is stopped, 1 is paused.

--- timer.py
+++ timer.py
@@ -7,25 +7,30 @@ root = Tk()
 root.title("Timer")
 root.geometry('350x200')
 root.resizable(True, True)
+is_counting = 0
 
 def update_timer():
     global is_counting
 
-    if time_left_var.get() > 0:
-        is_counting = 1
+    if time_left_var.get() > 0 and is_counting == 2:
         time_left_var.set(time_left_var.get() - 1)
         time_left_label.config(text=f"{str(timedelta(seconds=time_left_var.get()))}")
         root.after(1000, update_timer)  # Call again after 1 second
-    else:
+    elif is_counting != 1:
         is_counting = 0
 
 
 def start_timer():
     global is_counting
-    time_left_var.set(time_left_var.get() + time_var.get())
     if is_counting != 1:
+        time_left_var.set(time_left_var.get() + time_var.get())
+    if is_counting != 2:
+        is_counting = 2
         update_timer()
 
+def stop_timer():
+    global is_counting
+    is_counting = 1
 
 time_left_var = IntVar(root, 0)
 time_var = IntVar(root, 0)
@@ -33,11 +38,13 @@ time_label = Label(root, text = 'Input Time', font=('calibre',10,'bold'))
 time_entry = Entry(root, textvariable = time_var, font=('calibre',10,'normal'))
 time_left_label = Label(root, text = 'Time Left', font=('calibre',10,'bold'))
 start_button = Button(root, text="Start", command=start_timer)
+stop_button = Button(root, text="Stop", command=stop_timer)
 
 time_label.grid(row=0,column=0)
 time_entry.grid(row=0,column=1)
 time_left_label.grid(row=1,column=1)
 start_button.grid(row=2,column=1)
+stop_button.grid(row=2,column=2)
 
 update_timer()
 
