355fa31975eda8df2257c4b410f03eaf1627a0eb
Sun-G0D <47151235+Sun-G0D@users.noreply.github.com>
2025-02-01 16:15:05 -0800

    hours + minutes input
    now allows for inputting hours and minutes

--- timer.py
+++ timer.py
@@ -35,11 +35,10 @@ def update_timer():
         done("timer done")
 
 
-
 def start_timer():
     global is_counting
     if is_counting != 1:
-        time_left_var.set(time_left_var.get() + time_var.get())
+        time_left_var.set(time_left_var.get() + 3600 * hours_var.get() + 60 * minutes_var.get() + seconds_var.get())
     if is_counting != 2:
         is_counting = 2
         update_timer()
@@ -55,16 +54,26 @@ def reset_timer():
     time_left_label.config(text=f"{str(timedelta(seconds=time_left_var.get()))}")
 
 time_left_var = IntVar(root, 0)
-time_var = IntVar(root, 0)
-time_label = Label(root, text = 'Input Time', font=('calibre',10,'bold'))
-time_entry = Entry(root, textvariable = time_var, font=('calibre',10,'normal'))
+hours_var = IntVar(root, 0)
+minutes_var = IntVar(root, 0)
+seconds_var = IntVar(root, 0)
+hours_label = Label(root, text = 'Input Time', font=('calibre',10,'bold'))
+hours_entry = Entry(root, textvariable = hours_var, font=('calibre',10,'normal'), width=2)
+minutes_label = Label(root, text = ':', font=('calibre',10,'bold'))
+minutes_entry = Entry(root, textvariable = minutes_var, font=('calibre',10,'normal'), width=2)
+seconds_label = Label(root, text = ':', font=('calibre',10,'bold'))
+seconds_entry = Entry(root, textvariable = seconds_var, font=('calibre',10,'normal'), width=2)
 time_left_label = Label(root, text = 'Time Left', font=('calibre',10,'bold'))
 start_button = Button(root, text="Start", command=start_timer)
 stop_button = Button(root, text="Stop", command=stop_timer)
 reset_button = Button(root, text="Reset", command=reset_timer)
 
-time_label.grid(row=0,column=0)
-time_entry.grid(row=0,column=1)
+hours_label.grid(row=0,column=0)
+hours_entry.grid(row=0,column=1)
+minutes_label.grid(row=0,column=2)
+minutes_entry.grid(row=0,column=3)
+seconds_label.grid(row=0,column=4)
+seconds_entry.grid(row=0,column=5)
 time_left_label.grid(row=1,column=1)
 start_button.grid(row=2,column=1)
 stop_button.grid(row=2,column=2)
